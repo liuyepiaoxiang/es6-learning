92513afff806d5bb8ac995f8b1e23683b83772cf
liujia <liujia@192.168.0.73>
2017-05-10 20:53:22 +0800

    增加ES数组操作等

--- ECMAScript6/es6-compiled-compiled-compiled-compiled.js
+++ ECMAScript6/es6-compiled-compiled-compiled-compiled.js
@@ -34,20 +34,10 @@ s.startsWith('Hello'); // true
 s.endsWith('!'); // true
 s.includes('o'); // true
 
-//运算符
-//let b = 2 ** 3;
-
-let arrayLike = {
-    '0': 'a',
-    '1': 'b',
-    '2': 'c',
-    length: 3
-};
-// ES5的写法
-var arr1 = [].slice.call(arrayLike); // ['a', 'b', 'c']
-console.log(arr1);
-// ES6的写法
-let arr2 = Array.from(arrayLike); // ['a', 'b', 'c']
-console.log(arr2);
-
 //# sourceMappingURL=es6-compiled.js.map
+
+//# sourceMappingURL=es6-compiled-compiled.js.map
+
+//# sourceMappingURL=es6-compiled-compiled-compiled.js.map
+
+//# sourceMappingURL=es6-compiled-compiled-compiled-compiled.js.map
--- ECMAScript6/es6.js
+++ ECMAScript6/es6.js
@@ -32,3 +32,19 @@ var s = 'Hello world!';
 s.startsWith('Hello') // true
 s.endsWith('!') // true
 s.includes('o') // true
+
+//运算符
+//let b = 2 ** 3;
+
+let arrayLike = {
+    '0': 'a',
+    '1': 'b',
+    '2': 'c',
+    length: 3
+};
+// ES5的写法
+var arr1 = [].slice.call(arrayLike); // ['a', 'b', 'c']
+console.log(arr1)
+// ES6的写法
+let arr2 = Array.from(arrayLike); // ['a', 'b', 'c']
+console.log(arr2)
