0a3633677651b5385bc2f00936e632e533c25c03
liuyepiaoxiang <liujia19891217@126.com>
2017-07-16 22:57:04 +0800

    add python chap2

--- python/chap2/os.py
+++ python/chap2/os.py
@@ -2,6 +2,19 @@
 # -*-  coding:utf8 -*-
 
 import os
+import shelve
 cwd = os.getcwd();
 print('当前工作目录是：',os.getcwd());
-os.path.exists()
+
+fileName = open(cwd+'\\README.md')
+#fileContent = fileName.readlines()
+#print('文件内容：',fileContent)
+
+shelfFile = shelve.open('mydata')
+cats = ['白猫','黑猫','红猫']
+shelfFile['cats'] = cats
+shelfFile.close()
+
+shelfFile =shelve.open('mydata')
+print(type(shelfFile))
+print(shelfFile['cats'])
